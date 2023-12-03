6bc234ad3b3181e42fc344892f3ab7bb809a34be
Shubham <sk4957199@gmail.com>
2023-12-03 23:44:43 +0530

    footer

--- framer/src/components/Footer.jsx
+++ framer/src/components/Footer.jsx
@@ -3,10 +3,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="max-w-[1100px] flex flex-col justify-center   w-[100%] my-6 mx-auto py-6 ">
-      <h2 className="text-gray-600">© 2023 ManageWise, Inc.</h2>
-      <h2 className="text-gray-600 ">Developed By Jyoti Kumari</h2>
-    </div>
+    <>
+      <div className="w-[90vw] h-[1px] bg-gray-300 mx-auto"></div>
+      <div className="max-w-[1100px] flex  justify-between    w-[100%] my-6 mx-auto py-6 ">
+        <div>
+          <img
+            className="w-[200px]"
+            src="https://framerusercontent.com/images/tgELERqZ0nObn14bTi418qTbg.png?scale-down-to=512"
+            alt=""
+          />
+        </div>
+        <ul>
+          <li>Features</li>
+          <li>FAQ</li>
+          <li>Pricing</li>
+          <li>Testimonials</li>
+        </ul>
+      </div>
+      <div className="max-w-[1100px] flex  justify-between   w-[100%] my-6 mx-auto py-6 ">
+        <h2 className="text-gray-600">© 2023 ManageWise, Inc.</h2>
+        <h2 className="text-gray-600 ">Developed By Jyoti Kumari</h2>
+      </div>
+    </>
   );
 };
 
